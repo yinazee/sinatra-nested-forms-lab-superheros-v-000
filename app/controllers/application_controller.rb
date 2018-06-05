7792b5379781e79be80f6c806a185bfde777d280
Yina Zhu <yinagit@gmail.com>
2018-06-05 15:43:31 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,12 +14,12 @@ class App < Sinatra::Base
       @hero_name = []
       @hero_power =[]
       @hero_bio = []
-      @team_members = params[:team][:members]
+      # @team_members = params[:team][:members]
       binding.pry
-      @team_members.each do |member|
-        @hero_name << member.name
-        @hero_power << member.power
-        @hero_bio << member.bio
+      # @team_members.each do |member|
+      #   @hero_name << member.name
+      #   @hero_power << member.power
+      #   @hero_bio << member.bio
       end
       erb :team
 
