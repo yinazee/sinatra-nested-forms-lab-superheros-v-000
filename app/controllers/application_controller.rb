a87eb9987e49d5ea74699ad74588559a0dcfd45d
Yina Zhu <yinagit@gmail.com>
2018-06-05 16:04:06 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,9 +14,8 @@ class App < Sinatra::Base
       @hero_name = []
       @hero_power =[]
       @hero_bio = []
-      @team_members = params[:team][:members]
-      binding.pry
-      # @team_members.each do |member|
+      @team_members = params[:team][:hero]
+      @team_members.each do |hero|
       #   @hero_name << member[:name]
       #   @hero_power << member.power
       #   @hero_bio << member.bio
